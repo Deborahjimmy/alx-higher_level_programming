588c4494f30b10383d73e8a006605a2403c0f52f
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-06-29 02:24:34 +0100

    my check cycle file

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,32 +7,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	size_t bufsize = 2, i, elem = 0;
-	listint_t **buffer = malloc(bufsize * sizeof(listint_t*));
-	listint_t *current = list;
+        size_t bufsize = 2, i, elem = 0;
+        listint_t **buffer = malloc(bufsize * sizeof(listint_t*));
+        listint_t *current = list;
 
-	if (buffer == NULL)
-		return (-1);
+        if (buffer == NULL)
+                return (-1);
 
-	while (current)
-	{
-		for (i = 0; i < elem; i++)
-		{
-			if (current == buffer[i])
-			{
-				free(buffer);
-				return  (1);
-			}
-		}
-		if (elem == bufsize)
-		{
-			bufsize += 2;
-			buffer = realloc(buffer, bufsize * sizeof(listint_t*));
-		}
-		buffer[elem] = current;
-		elem++;
-		current = current->next;
-	}
-	free(buffer);
-	return (0);
-}
+        while (current)
+        {
+                for (i = 0; i < elem; i++)
+                {
+                        if (current == buffer[i])
+
