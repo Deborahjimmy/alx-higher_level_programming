cc8e22ca20f46bdefc24bb7d2fa4bb85e85b0c3d
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-06-30 23:46:19 +0100

    update 4-hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in names:
+    namez = dir(hidden_4)
+    for name in namez:
         if name.startswith("__") is False:
             print("{}".format(name))
