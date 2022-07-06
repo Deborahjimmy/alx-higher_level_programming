9891b9d06691dbcdd883153b48348649d26fb397
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-07-06 18:01:07 +0100

    updated data_structure

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 
 def square_matrix_simple(matrix=[]):
     """computes the square value of all integers of a matrix."""
-    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
+    return [list(map(lambda x: x **2, row)) for row in matrix]
         
