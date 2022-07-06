f7b6090c3a78cfd6b49a6b9754f54157e604952a
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-07-06 12:55:21 +0100

    more data structure

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    matrix= [
+        [0, 1, 2, 3, 4],
+        [5, 6, 7, 8, 9], ]
+    for x in range(10):
+        squares .append(x**2)
+        
