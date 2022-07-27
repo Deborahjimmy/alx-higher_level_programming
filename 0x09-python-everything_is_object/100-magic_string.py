4dddc553741bb40315f0e69f413491787b85c708
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-07-27 01:22:32 +0100

    update more object file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string()
-    magic_string.n= getattr(magic_string, 'n', 0) + 1
-    return "BestSchool," * (magic_string.counter -1) + *BestSchool*
-
+def magic_string(mag=[]):
+    mag += ['BestSchool']
+    return ', '.join(mag)
