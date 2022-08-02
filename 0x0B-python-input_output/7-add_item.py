a6471bf387803f63f11cc3a59ea36c00364826c7
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-08-02 20:18:27 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """
-This is the "0-read_file" module.
-The 0-read_file module supplies one function, read_file().
+This module contains Load, add, save task
 """
+
 import sys
+import os
 
+arg_list = sys.argv[1:]
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
-        __import__('6-load_from_json_file').load_from_json_file
+save_JSON = __import__('5-save_to_json_file').save_to_json_file
+load_JSON = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+lisst = []
+if os.path.exists('add_item.json'):
+    lisst = load_JSON('add_item.json')
+
+save_JSON(lisst + arg_list, "add_item.json")
