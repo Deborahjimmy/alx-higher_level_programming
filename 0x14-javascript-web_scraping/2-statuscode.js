4864f10c12cd3936ed3c7ec371f23a85854e29fe
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-11-16 03:19:57 +0100

    update javascript-web_scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,9 @@
-re('request');
-const endpoint = process.argv[2];
+#!/usr/bin/node
 
-request(endpoint, (err, res, body) => {
-  if (!err) console.log('code:', res.statusCode);
-});
+const request = require('request');
+const url = process.argv[2];
+request
+  .get(url)
+  .on('response', function (response) {
+    console.log('code: ' + response.statusCode);
+  });
