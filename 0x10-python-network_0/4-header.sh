572f4addbb986b0e7393c829ec40dc402b96e2e2
Deborahjimmy <deborahjimmy.dj@gmail.com>
2023-01-18 20:51:26 +0100

    update python network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a get request with custome data and display body
-curl -s "$1" -X GET -H 'X-HolbertonSchool-User-Id: 98' 
+# curl sends GET req to URL, displays response body
+curl -sH "X-School-User-Id: 98" "$1" 
